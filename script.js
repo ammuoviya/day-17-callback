8d0d9404892e83afce138bb0261f55f3a2e219ad
Oviya <ammuoviya19@gmail.com>
2023-10-12 19:58:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+function hell()
+{
+setTimeout(function() {
+    document.write("10<br>");
+     setTimeout(function() {
+       document.write("9<br>");
+       setTimeout(function() {
+         document.write("8<br>");
+         setTimeout(function() {
+           document.write("7<br>");
+           setTimeout(function() {
+             document.write("6<br>");
+             setTimeout(function() {
+               document.write("5<br>");
+               setTimeout(function() {
+                 document.writeln("4<br>");
+                 setTimeout(function() {
+                   document.writeln("3<br>");
+                   setTimeout(function() {
+                     document.write("2<br>");
+                     setTimeout(function() {
+                       document.write("1<br>");
+                       setTimeout(function() {
+                         document.write("Happy Independence Day!!!");
+                       }, 1000);
+                     }, 1000);
+                   }, 1000);
+                 }, 1000);
+               }, 1000);
+             }, 1000);
+           }, 1000);
+         }, 1000);
+       }, 1000);
+     }, 1000);
+   }, 1000);
+}
+   
